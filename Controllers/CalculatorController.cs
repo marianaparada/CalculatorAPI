bd72776bcbb0ab89aa9b2ad32d9b98b6a3fce2cb
Mariana Parada <a2024111530@estudiantes.upsa.edu.bo>
2025-08-20 19:51:33 -0400

    agregar endopoint GET/sum

--- Controllers/CalculatorController.cs
+++ Controllers/CalculatorController.cs
@@ -1,6 +1,15 @@
-namespace CalculatorAPI.Controllers;
+using Microsoft.AspNetCore.Mvc;
 
-public class CalculatorController
+namespace CalculatorAPI.Controllers
 {
-    
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CalculatorController : ControllerBase
+    {
+        [HttpGet("sum")]
+        public ActionResult<double> Sum([FromQuery] double a, [FromQuery] double b)
+        {
+            return Ok(a + b);
+        }
+    }
 }
